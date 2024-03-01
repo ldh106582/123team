d50285c1a97d22ceed09ec586b889ad5d8067671
dohyeonlee <ehgus190@naver.com>
2024-03-01 21:10:51 +0900

    2024-03-01 이미지 완료

--- main/java/com/springmvc/repository/AjaxRepositoryImpl.java
+++ main/java/com/springmvc/repository/AjaxRepositoryImpl.java
@@ -5,7 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
+// 224-03-01
 @Repository
 public class AjaxRepositoryImpl implements AjaxRepository{
 
